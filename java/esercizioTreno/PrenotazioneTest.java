fbf7b0acdd0f25d5389ef53e6e04069db6768a93
spikerola <markesin@gmail.com>
2016-04-13 17:36:30 +0200

    Create PrenotazioneTest.java

---
+++ java/esercizioTreno/PrenotazioneTest.java
@@ -0,0 +1,25 @@
+public class PrenotazioneTest {
+    public static void main(String[] args) {
+        Cliente clienti[] = new Cliente[100];
+        Treno t1  = new Treno(1 , 50, 50);
+
+        int i = 0;
+        // Istanzio 5 clienti ricchi che prenotano 10 posti ciascuno
+        for(; i<5; i++) clienti[i] = new Cliente(10, false,t1);
+        // Istanzio 9 clienti poveri che prenotano 4 posti ciascuno
+        for(; i<9; i++) clienti[i] = new Cliente(4, true,t1);
+        // Istanzio 10 clienti poveri che prenotano -6 posti ciascuno
+        for(; i<10; i++) clienti[i] = new Cliente(-6, true,t1);
+
+
+        for (int j = 0; j < i; j++) clienti[j].start();
+
+        try {
+            Thread.sleep(2000);
+        } catch(InterruptedException ex) {
+            System.out.println(ex);
+        }
+
+        System.out.println(t1);
+    } // main
+}
