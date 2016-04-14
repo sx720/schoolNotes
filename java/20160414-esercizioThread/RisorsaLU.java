6c629d91e94e4abf217cdd83c907ec3e05b3766b
spikerola <markesin@gmail.com>
2016-04-14 17:51:43 +0200

    Create RisorsaLU.java

---
+++ java/20160414-esercizioThread/RisorsaLU.java
@@ -0,0 +1,66 @@
+/*
+Definire la classe RisorsaLU che gestisce la prenotazione di una risorsa mediante
+il protocollo lock/unlock "generico" e che possiede le seguenti operazioni pubbliche:
+~ Costruttore, che ha due parametri: il nome della risorsa;
+   il numero massimo di processi che possono preno-
+tare la risorsa.
+~ getId(), che ritorna il nome della risorsa.
+~ lock e unlock descritte precedentemente,
+   e che devono essere definite a seconda della politica di gestione della prenotazione.
+RisorsaLU deve opportunamente memorizzare quali tra gli nProc hanno effettivamente prenotato la risorsa.
+*/
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
+
+public class RisorsaLU {
+    private String nomeRis;
+    private int nProc = 0;
+    private int[] nomiProcessi;
+    private final int maxProc;
+
+    private final Lock l = new ReentrantLock();
+    private final Condition queueNotFull = l.newCondition();
+    // private final Condition queueNotEmpty = l.newCondition();
+
+    public RisorsaLU(String nomeRis, int nProc) {
+        this.nomeRis = nomeRis;
+        this.maxProc = nProc;
+        nomiProcessi = new int[nProc];
+        for (int i = 0; i < nProc; i++) nomiProcessi[i] = -1;
+    }
+
+    public String getId() {
+        return nomeRis;
+    }
+
+    public void lock(int iProc) throws InterruptedException {
+        try {
+            while (nProc == maxProc) {
+                System.out.println(this.getId() + " sta aspettando il lock..");
+                queueNotFull.await();
+            }
+        } finally {
+            for (int i : nomiProcessi) {
+                if (i == -1) {
+                    i = iProc;
+                    break;
+                }
+            }
+            nProc++;
+            l.lock();
+        }
+    }
+
+    public void unlock(int iProc) {
+        l.unlock();
+        for (int i = 0; i < maxProc; i++) {
+            if (nomiProcessi[i] == iProc) {
+                nomiProcessi[i] = -1;
+                break;
+            }
+        }
+        nProc--;
+        queueNotFull.signalAll();
+    }
+}
